1df3259f20de68866f9dc11f66173e54a5b372ec
wangwenjie <1772755454@qq.com>
2020-04-23 21:31:59 +0800

    vuex 模块化

--- src/components/VuexTest.vue
+++ src/components/VuexTest.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h3>vuex test</h3>
-    <p>{{$store.state.count}}</p>
-    <p>{{$store.getters.score}}</p>
+    <p>{{$store.state.a.count}}</p>
+    <p>{{$store.getters['a/score'].score}}</p>
     <button @click="add">add</button>
     <button @click="asyncAdd">asyncAdd</button>
   </div>
@@ -11,10 +11,10 @@
 export default {
   methods:{
     add(){
-      this.$store.commit('add',2)
+      this.$store.commit('a/add',2)
     },
     async asyncAdd(){
-      const result = await this.$store.dispatch('asyncAdd')
+      const result = await this.$store.dispatch('a/asyncAdd')
       if(result.ok===1){
         alert('操作成功！')
       }
