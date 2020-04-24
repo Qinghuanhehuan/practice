5a3de9c2ccdeeb7785308e9845dfb989e0b13aab
wangwenjie <1772755454@qq.com>
2020-04-24 22:50:13 +0800

    使用express返回数据请求

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const port = 7070
 const title = '最佳实践'
 const path = require('path')
 // 处理post请求参数
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 function resolve(dir){
   return path.join(__dirname,dir)
@@ -30,34 +30,34 @@ module.exports={
     },
     // 配置mock接口
     // app是express的实例
-    before: app => {
-        // 处理post参数
-      app.use(bodyParser.json());
-      app.post("/dev-api/user/login", (req, res) => {
-        const { username } = req.body;
-        if (username === "admin" || username === "wwj") {
-          res.json({
-            code: 1,
-            data: username
-          });
-        } else {
-          res.json({
-            code: 10204,
-            message: "用户名或密码错误"
-          });
-        }
-      });
+    // before: app => {
+    //     // 处理post参数
+    //   app.use(bodyParser.json());
+    //   app.post("/dev-api/user/login", (req, res) => {
+    //     const { username } = req.body;
+    //     if (username === "admin" || username === "wwj") {
+    //       res.json({
+    //         code: 1,
+    //         data: username
+    //       });
+    //     } else {
+    //       res.json({
+    //         code: 10204,
+    //         message: "用户名或密码错误"
+    //       });
+    //     }
+    //   });
 
-      // 假设此时拿到的令牌是合法的
-      app.get("/dev-api/user/info", (req, res) => {
-        const auth = req.headers["authorization"];
-        const roles = auth.split(" ")[1] === "admin" ? ["admin"] : ["editor"];
-        res.json({
-          code: 1,
-          data: roles
-        });
-      });
-    }
+    //   // 假设此时拿到的令牌是合法的
+    //   app.get("/dev-api/user/info", (req, res) => {
+    //     const auth = req.headers["authorization"];
+    //     const roles = auth.split(" ")[1] === "admin" ? ["admin"] : ["editor"];
+    //     res.json({
+    //       code: 1,
+    //       data: roles
+    //     });
+    //   });
+    // }
   },
   configureWebpack:{
     name:title
